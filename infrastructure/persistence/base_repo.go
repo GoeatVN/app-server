f9d7ea938ba7f333b92ca8d71f1cfa62174c0bf5
admin <thongproleminh@gmail.com>
2024-09-24 23:06:19 +0700

    fix base repo

--- infrastructure/persistence/base_repo.go
+++ infrastructure/persistence/base_repo.go
@@ -6,27 +6,27 @@ import (
 	"gorm.io/gorm"
 )
 
-//gorm generic repository
+//gorm generic baserepository
 
-type repository[T any] struct {
+type baserepository[T any] struct {
 	db *gorm.DB
 }
 
-func NewRepository[T any](db *gorm.DB) *repository[T] {
-	return &repository[T]{
+func NewRepository[T any](db *gorm.DB) *baserepository[T] {
+	return &baserepository[T]{
 		db: db,
 	}
 }
 
-func (r *repository[T]) Add(entity *T, ctx context.Context) error {
+func (r *baserepository[T]) Add(entity *T, ctx context.Context) error {
 	return r.db.WithContext(ctx).Create(&entity).Error
 }
 
-func (r *repository[T]) AddAll(entity *[]T, ctx context.Context) error {
+func (r *baserepository[T]) AddAll(entity *[]T, ctx context.Context) error {
 	return r.db.WithContext(ctx).Create(&entity).Error
 }
 
-func (r *repository[T]) GetById(id int, ctx context.Context) (*T, error) {
+func (r *baserepository[T]) GetById(id int, ctx context.Context) (*T, error) {
 	var entity T
 	err := r.db.WithContext(ctx).Model(&entity).Where("id = ? AND is_active = ?", id, true).FirstOrInit(&entity).Error
 	if err != nil {
@@ -36,13 +36,13 @@ func (r *repository[T]) GetById(id int, ctx context.Context) (*T, error) {
 	return &entity, nil
 }
 
-func (r *repository[T]) Get(params *T, ctx context.Context) *T {
+func (r *baserepository[T]) Get(params *T, ctx context.Context) *T {
 	var entity T
 	r.db.WithContext(ctx).Where(&params).FirstOrInit(&entity)
 	return &entity
 }
 
-func (r *repository[T]) GetAll(ctx context.Context) (*[]T, error) {
+func (r *baserepository[T]) GetAll(ctx context.Context) (*[]T, error) {
 	var entities []T
 	err := r.db.WithContext(ctx).Find(&entities).Error
 	if err != nil {
@@ -51,7 +51,7 @@ func (r *repository[T]) GetAll(ctx context.Context) (*[]T, error) {
 	return &entities, nil
 }
 
-func (r *repository[T]) Where(params *T, ctx context.Context) (*[]T, error) {
+func (r *baserepository[T]) Where(params *T, ctx context.Context) (*[]T, error) {
 	var entities []T
 	err := r.db.WithContext(ctx).Where(&params).Find(&entities).Error
 	if err != nil {
@@ -60,20 +60,20 @@ func (r *repository[T]) Where(params *T, ctx context.Context) (*[]T, error) {
 	return &entities, nil
 }
 
-func (r *repository[T]) Update(entity *T, ctx context.Context) error {
+func (r *baserepository[T]) Update(entity *T, ctx context.Context) error {
 	return r.db.WithContext(ctx).Save(&entity).Error
 }
 
-func (r repository[T]) UpdateAll(entities *[]T, ctx context.Context) error {
+func (r baserepository[T]) UpdateAll(entities *[]T, ctx context.Context) error {
 	return r.db.WithContext(ctx).Save(&entities).Error
 }
 
-func (r *repository[T]) Delete(id int, ctx context.Context) error {
+func (r *baserepository[T]) Delete(id int, ctx context.Context) error {
 	var entity T
 	return r.db.WithContext(ctx).FirstOrInit(&entity).UpdateColumn("is_active", false).Error
 }
 
-func (r *repository[T]) SkipTake(skip int, take int, ctx context.Context) (*[]T, error) {
+func (r *baserepository[T]) SkipTake(skip int, take int, ctx context.Context) (*[]T, error) {
 	var entities []T
 	err := r.db.WithContext(ctx).Offset(skip).Limit(take).Find(&entities).Error
 	if err != nil {
@@ -82,14 +82,14 @@ func (r *repository[T]) SkipTake(skip int, take int, ctx context.Context) (*[]T,
 	return &entities, nil
 }
 
-func (r *repository[T]) Count(ctx context.Context) int64 {
+func (r *baserepository[T]) Count(ctx context.Context) int64 {
 	var entity T
 	var count int64
 	r.db.WithContext(ctx).Model(&entity).Count(&count)
 	return count
 }
 
-func (r *repository[T]) CountWhere(params *T, ctx context.Context) int64 {
+func (r *baserepository[T]) CountWhere(params *T, ctx context.Context) int64 {
 	var entity T
 	var count int64
 	r.db.WithContext(ctx).Model(&entity).Where(&params).Count(&count)

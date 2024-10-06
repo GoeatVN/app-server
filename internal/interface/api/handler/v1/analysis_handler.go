f95c8ef19fb913ebb5fd165257f136427c0bf1b3
admin <thongproleminh@gmail.com>
2024-10-06 22:40:27 +0700

    Refactor launch.json: Update program path

--- internal/interface/api/handler/v1/analysis_handler.go
+++ internal/interface/api/handler/v1/analysis_handler.go
@@ -19,6 +19,9 @@ func NewSoilAnalysisHandler(soilAnalysisService soil_analysis.SoilAnalysisServic
 
 // Login handles user login
 func (h *SoilAnalysisHandler) SaveSoilAnalysis(c *gin.Context) {
+	/*
+		{"request":{"id":""},"garden":{"id":"","company_code":"CT001","farm_code":"FRO001","plot_code":"LO001","plot_area":1000,"soil_type_code":"2","age_tree":10,"planting_year":2008,"root_depth":30,"growth_status":true,"standard_code":true,"ground_cover":true,"leaf_color_code":"XD","variety_code":"CS001","standard_percentage":70},"soils":[{"id":"","parameter_code":"PH","parameter_value":1.2},{"id":"","parameter_code":"NH3","parameter_value":1},{"id":"","parameter_code":"NO3","parameter_value":0.7},{"id":"","parameter_code":"P2O5","parameter_value":100},{"id":"","parameter_code":"K2O","parameter_value":60},{"id":"","parameter_code":"MG","parameter_value":65},{"id":"","parameter_code":"CU","parameter_value":70},{"id":"","parameter_code":"ZN","parameter_value":1.2}],"user_process":"1"}
+	*/
 
 	var request analysis_model.SoilAnalysisRequest
 
